00646ec9b3e3d678ab7a45a2b58f1112f1900886
nandhu10jishnu <124574524+nandhu10jishnu@users.noreply.github.com>
2023-04-21 22:48:19 +0530

    Add files via upload

---
+++ EmployeeAppFrontend-main/src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
+        <div className="container-fluid">
+          <Link to="/"  style={{textDecoration:"none",color:"black"}}>Empolyee</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ">
+              <li className="nav-item ">
+              <Link to="/"  style={{textDecoration:"none",color:"white"}}>Home</Link>
+              </li>
+              <li className="nav-item ">
+              <Link to="/form"  style={{textDecoration:"none",color:"black"}}>Add Empolyee</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
